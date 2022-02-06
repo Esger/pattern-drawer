778ba13d6fd4fdfed708bddf35201f6e645525c9
Esger <esgerj@gmail.com>
2022-02-06 16:28:30 +0100

    chain doet het ook

--- src/resources/elements/the-canvas.js
+++ src/resources/elements/the-canvas.js
@@ -3,33 +3,31 @@ import { paper } from 'paper';
 @inject(paper)
 export class TheCanvas {
     @bindable value;
-    constructor(paper) {
-        this._paper = paper;
+    constructor() {
         // this._element = element;
     }
 
     attached() {
-        this._paper = paper;
         const canvas = document.getElementById('patternCanvas');
-        this._paper.setup(canvas);
-        this._demo();
+        paper.setup(canvas);
         this._chain();
-        console.log(this._paper);
+        // this._demo();
+        console.log(paper);
     }
 
     _demo() {
         // Create a Paper.js Path to draw a line into it:
-        const path = new this._paper.Path();
+        const path = new paper.Path();
         // Give the stroke a color
         path.strokeColor = 'black';
-        const start = new this._paper.Point(100, 100);
+        const start = new paper.Point(100, 100);
         // Move to start and draw a line from there
         path.moveTo(start);
         // Note that the plus operator on Point objects does not work
         // in JavaScript. Instead, we need to call the add() function:
         path.lineTo(start.add([200, -50]));
         // Draw the view now:
-        this._paper.view.draw();
+        paper.view.draw();
     }
 
     _chain() {
@@ -40,40 +38,41 @@ export class TheCanvas {
         const points = 25;
 
         // The distance between the points:
-        const length = 35;
+        this.segmentLength = 35;
 
-        this._path = new this._paper.Path({
-            strokeColor: '#e4141b',
+        this._path = new paper.Path({
+            strokeColor: 'crimson',
             strokeWidth: 20,
             strokeCap: 'round'
         });
 
-        var start = this._paper.view.center / [10, 1];
+        var start = paper.view.center;//.divide([10, 1]);
         for (var i = 0; i < points; i++)
-            this._path.add(start + new this._paper.Point(i * length, 0));
-    }
+            this._path.add(start + new paper.Point(i * this.segmentLength, 0));
 
-    onMouseMove(event) {
-        this._path.firstSegment.point = event.point;
-        for (var i = 0; i < this._path.points - 1; i++) {
-            var segment = this._path.segments[i];
-            var nextSegment = segment.next;
-            var vector = segment.point - nextSegment.point;
-            vector.length = length;
-            nextSegment.point = segment.point - vector;
+        this.onMouseMove = (event) => {
+            this._path.firstSegment.point = [event.offsetX, event.offsetY];
+            for (var i = 0; i < this._path.segments.length - 1; i++) {
+                var segment = this._path.segments[i];
+                var nextSegment = segment.next;
+                var vector = segment.point.subtract(nextSegment.point);
+                vector.length = this.segmentLength;
+                nextSegment.point = segment.point.subtract(vector);
+            }
+            this._path.smooth({ type: 'continuous' });
         }
-        this._path.smooth({ type: 'continuous' });
+
+        this.onMouseDown = (event) => {
+            this._path.fullySelected = true;
+            this._path.strokeColor = '#e08285';
+        }
+
+        this.onMouseUp = (event) => {
+            this._path.fullySelected = false;
+            this._path.strokeColor = '#e4141b';
+        }
     }
 
-    onMouseDown(event) {
-        this._path.fullySelected = true;
-        this._path.strokeColor = '#e08285';
-    }
-
-    onMouseUp(event) {
-        this._path.fullySelected = false;
-        this._path.strokeColor = '#e4141b';
-    }
 
     valueChanged(newValue, oldValue) {
         //
