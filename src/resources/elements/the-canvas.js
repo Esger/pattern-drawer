b48243b380b5566ccad351a58d65def6c35a476b
Esger <esgerj@gmail.com>
2022-02-13 21:39:50 +0100

    no draw one more copy

--- src/resources/elements/the-canvas.js
+++ src/resources/elements/the-canvas.js
@@ -99,7 +99,7 @@ export class TheCanvas {
                     strokeWidth: 20,
                     strokeCap: 'round'
                 });
-                var start = paper.view.center.divide([10, 1]);
+                var start = paper.view.center.divide([11, 1]);
                 for (var i = 0; i < points; i++)
                     path.add(start + new paper.Point(i * segmentLength, 0));
                 path.name = 'original';
@@ -167,7 +167,7 @@ export class TheCanvas {
             [-1 / 2, -1 / 4, 0, 1 / 4, 1 / 2],
             [-5 / 8, - 3 / 8, -1 / 8, 1 / 8, 3 / 8, 5 / 8],
             [-6 / 9, - 4 / 9, -2 / 9, 0, 2 / 9, 4 / 9, 6 / 9],
-            [-7 / 10, - 5 / 10, -3 / 10, -1 / 10, 1 / 10, 3 / 10, 5 / 10, 7 / 10],
+            [-7 / 11, - 5 / 11, -3 / 11, -1 / 11, 1 / 11, 3 / 11, 5 / 11, 7 / 11],
         ];
         const maxCopies = offsets[offsets.length - 1].length;
         const oldCountX = this._offsets[0].length;
