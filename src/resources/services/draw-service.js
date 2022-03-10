9bd12b9b9094da67e38382baa7156af6a911d1ca
Esger <esgerj@gmail.com>
2022-03-11 00:24:30 +0100

    Undo, Select & Delete, Line length setting

--- src/resources/services/draw-service.js
+++ src/resources/services/draw-service.js
@@ -5,6 +5,7 @@ import { EventAggregator } from 'aurelia-event-aggregator';
 export class DrawService extends AbstractDrawService {
     _drawTool;
     _offsetGroups = [];
+    _selection = [];
 
     constructor(eventAggregator) {
         super(eventAggregator);
@@ -35,24 +36,43 @@ export class DrawService extends AbstractDrawService {
 
         this._drawTool.onMouseDown = (event) => {
             penDown = !penDown;
-            if (penDown) {
-                this._offsetGroups.forEach((offsetGroup, index) => {
-                    currentPathName = ('path-' + event.timeStamp).replace('.', '').substring(0, 12);
-                    newPath = makeNewPath(currentPathName + index);
-                    newPath.position = event.point.add(this._offsetsFlat[index]);
-                    newPath.add(new paper.Point(newPath.position));
-                    offsetGroup.addChild(newPath);
+            this._offsetGroups.forEach(offsetgroup => {
+                const hitResult = offsetgroup.hitTest(event.point, {
+                    fill: false, stroke: true, segments: false, class: Path
                 });
-            }
+                if (hitResult) hitResult.item.fullySelected = !hitResult.item.fullySelected;
+            });
+            this._offsetGroups.forEach((offsetGroup, index) => {
+                currentPathName = ('path-' + event.timeStamp).replace('.', '').substring(0, 12);
+                newPath = makeNewPath(currentPathName + index);
+                newPath.position = event.point.add(this._offsetsFlat[index]);
+                newPath.add(new paper.Point(newPath.position));
+                offsetGroup.addChild(newPath);
+            });
         }
 
         this._drawTool.onMouseMove = (event) => {
             if (penDown) {
-                // path.fullySelected = true;
                 this._offsetGroups.forEach((offsetGroup, index) => {
                     const currentPath = offsetGroup.children[currentPathName + index];
                     const newPoint = new paper.Point(event.point.add(this._offsetsFlat.flat(1)[index]));
                     currentPath.add(newPoint);
+                });
+            } else {
+                // show selection hovered path
+                this._offsetGroups.forEach((offsetgroup, index) => {
+                    const hitResult = offsetgroup.hitTest(event.point, {
+                        fill: false, stroke: true, segments: false, class: Path
+                    });
+                    if (hitResult) {
+                        hitResult.item.selected = true;
+                        this._selection[index].add(hitResult.item);
+                    } else {
+                        this._selection[index].forEach(path => {
+                            if (!path.fullySelected) path.selected = false;
+                        });
+                        this._selection[index].clear();
+                    }
                 });
             }
         }
@@ -64,7 +84,6 @@ export class DrawService extends AbstractDrawService {
                 const currentPath = offsetGroup.children[currentPathName + index];
                 currentPath.simplify();
             })
-            // path.fullySelected = false;
         }
     }
 
@@ -75,6 +94,7 @@ export class DrawService extends AbstractDrawService {
         this._offsetsFlat.forEach(offset => {
             const pathGroup = new paper.Group();
             this._offsetGroups.push(pathGroup);
+            this._selection.push(new Set());
         });
     }
 
@@ -86,7 +106,18 @@ export class DrawService extends AbstractDrawService {
     }
 
     _erase() {
-        super._erase();
+        if (paper.project.selectedItems.length) {
+            this._offsetGroups.forEach(offsetGroup => {
+                const pathsToHide = offsetGroup.children.filter(path => path.fullySelected);
+                pathsToHide.forEach(path => {
+                    path.remove();
+                });
+            });
+            this._drawTool.activate();
+        } else {
+            super._erase();
+        }
+
     }
 
 }
